7ec5dfc55cae526f81e9e437e07ec9964cfa9ed0
Ewan Drummond <45878916+edrumm@users.noreply.github.com>
2022-01-27 00:45:45 +0000

    first commit, getting setup

---
+++ cpp-ansi-colours/cpp-ansi-colours/Colours.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef COLOURS_H
+#define COLOURS_H
+
+// Placeholder values
+namespace Colours {
+	const int RED = 0;
+	// ...
+}
+
+#endif
---
+++ cpp-ansi-colours/cpp-ansi-colours/cpp-ansi-colours.cpp
@@ -0,0 +1,15 @@
+// cpp-ansi-colours.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Colours.h"
+
+int main()
+{
+    std::cout << Colours::RED << std::endl;
+
+    // Testing starts HERE
+    // ...
+
+    return 0;
+}
